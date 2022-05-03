8c6231ad6439fb15549a4a65bee5720751bfefcb
Marcelo Orcelam <104565438+micuentadegit@users.noreply.github.com>
2022-05-03 13:49:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 # created the rules python file
 
-streamlit.title('My mom\'s New Healthy Diner')
+streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
